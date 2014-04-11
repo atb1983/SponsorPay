343a35ef4a1056a7b5da0139033b31794d64730c
Alex Núñez <atb1983@gmail.com>
2014-04-11 20:09:09 +0200

    Initial Commit

---
+++ SponsorPlay/SPAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  SPAppDelegate.h
+//  SponsorPlay
+//
+//  Created by Alex Núñez on 11/04/14.
+//  Copyright (c) 2014 Alex Franco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ SponsorPlay/SPMasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  SPMasterViewController.h
+//  SponsorPlay
+//
+//  Created by Alex Núñez on 11/04/14.
+//  Copyright (c) 2014 Alex Franco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface SPMasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
