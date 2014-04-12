726fa32e74b563eea983302327fd3bb294d43d66
Alex Núñez <atb1983@gmail.com>
2014-04-12 07:22:35 +0200

    Added the SpResponse class

---
+++ SponsorPlay/Model/SPRespose.m
@@ -0,0 +1,13 @@
+//
+//  SPRespose.m
+//  SponsorPlay
+//
+//  Created by Alex Núñez on 12/04/14.
+//  Copyright (c) 2014 Alex Franco. All rights reserved.
+//
+
+#import "SPRespose.h"
+
+@implementation SPRespose
+
+@end
