46bda2841bd6e4c2b402e1c730010070fb36c84f
Alex Núñez <atb1983@gmail.com>
2014-04-13 16:58:36 +0200

    Unit Tests

---
+++ SponsorPay Tests/SponsorPay_Tests.m
@@ -0,0 +1,34 @@
+//
+//  SponsorPay_Tests.m
+//  SponsorPay Tests
+//
+//  Created by Alex Núñez on 13/04/14.
+//  Copyright (c) 2014 Alex Franco. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SponsorPay_Tests : XCTestCase
+
+@end
+
+@implementation SponsorPay_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
